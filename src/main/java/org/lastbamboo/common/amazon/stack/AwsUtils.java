a42970f7d08a66d0e5c55151939747bfb6d7e2d7
Adam Fisk <a@littleshoot.org>
2012-07-06 18:17:02 -0700

    Added fallback option for missing values and reformatted

--- src/main/java/org/lastbamboo/common/amazon/stack/AwsUtils.java
+++ src/main/java/org/lastbamboo/common/amazon/stack/AwsUtils.java
@@ -14,84 +14,77 @@ import org.slf4j.LoggerFactory;
 /**
  * Utility methods for Amazon web services.
  */
-public class AwsUtils
-    {
+public class AwsUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(AwsUtils.class);
 
-    private AwsUtils()
-        {
-        
-        }
-    
-    public static String getAccessKey() throws IOException
-        {
-        return getStringValue("accessKey");
-        }
+    private AwsUtils() {}
 
-    public static String getAccessKeyId() throws IOException
-        {
+    public static String getAccessKey() throws IOException {
+        return getStringValue("accessKey");
+    }
+
+    public static String getAccessKeyId() throws IOException {
         return getStringValue("accessKeyId");
-        }
-    
-    public static String getUrlBase() throws IOException 
-        {
-        return getStringValue("urlBase");
-        }
-    
-    public static String getSecureUrlBase() throws IOException 
-        {
-        return getStringValue("secureUrlBase");
-        }
-    
-    private static String getStringValue(final String key) throws IOException 
-        {
+    }
+
+    public static String getUrlBase() throws IOException {
+        return getStringValue("urlBase", "http://s3.amazonaws.com/");
+    }
+
+    public static String getSecureUrlBase() throws IOException {
+        return getStringValue("secureUrlBase", "https://s3.amazonaws.com:443/");
+    }
+
+    private static String getStringValue(final String key) throws IOException {
         final Properties props = locatePropsFile();
         final String prop = props.getProperty(key);
-        if (StringUtils.isBlank(prop))
-            {
-            throw new IOException("Could not find value for key: "+key);
-            }
-        return prop;
+        if (StringUtils.isBlank(prop)) {
+            throw new IOException("Could not find value for key: " + key);
         }
-    
-    private static Properties locatePropsFile() throws IOException
-        {
+        return prop;
+    }
+
+    private static String getStringValue(final String key, final String fallback)
+            throws IOException {
+        final Properties props = locatePropsFile();
+        final String prop = props.getProperty(key);
+        if (StringUtils.isBlank(prop)) {
+            return fallback;
+        }
+        return prop;
+    }
+
+    private static Properties locatePropsFile() throws IOException {
         final File lsDir = new File(SystemUtils.USER_HOME, ".littleshoot");
         final File home = new File(lsDir, "littleshoot.properties");
-        if (home.isFile())
-            {
+        if (home.isFile()) {
             return createPropsFile(home);
-            }
-        final File etc = new File("/etc/littleshoot/littleshoot.properties");
-        if (etc.isFile())
-            {
-            return createPropsFile(etc);
-            }
-        throw new IOException(
-            "Could not find props file in "+lsDir+" or in "+home);
         }
+        final File etc = new File("/etc/littleshoot/littleshoot.properties");
+        if (etc.isFile()) {
+            return createPropsFile(etc);
+        }
+        throw new IOException("Could not find props file in " + lsDir
+                + " or in " + home);
+    }
 
-    private static Properties createPropsFile(final File file) 
-        throws IOException
-        {
+    private static Properties createPropsFile(final File file)
+            throws IOException {
         final Properties props = new Properties();
         final InputStream is = new FileInputStream(file);
         props.load(is);
+        is.close();
         return props;
-        }
+    }
 
-    public static boolean hasPropsFile()
-        {
-        try
-            {
+    public static boolean hasPropsFile() {
+        try {
             locatePropsFile();
             return true;
-            }
-        catch (final IOException e)
-            {
+        } catch (final IOException e) {
             LOG.debug("No props file found", e);
             return false;
-            }
         }
     }
+}
